b7b9b79686cede989caf46b7908e5719d309496a
Nathan Grey <greynng@gmail.com>
2014-05-07 16:24:59 -0700

    Revert "add a comment model"
    This reverts commit 0ca16edfee50536bb6928ef9eb58dcaf051e6659.
    
    missed a commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507231624) do
+ActiveRecord::Schema.define(version: 20140507220042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: true do |t|
-    t.datetime "date"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -45,7 +38,6 @@ ActiveRecord::Schema.define(version: 20140507231624) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
