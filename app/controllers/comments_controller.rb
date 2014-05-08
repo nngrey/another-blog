56524d5066f956e0b4bb63015267e58c64529995
Nathan Grey <greynng@gmail.com>
2014-05-08 16:30:07 -0700

    add delete button to comments WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,6 +25,29 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice]="Comment updated."
+      redirect_to post_path(@comment.post_id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    id = @comment.post_id
+    @comment.destroy
+    flash[:notice] = "Comment deleted."
+    redirect_to post_path(@comment.post_id)
+  end
+
 private
 
   def comment_params
