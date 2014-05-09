44cdef0a6536eeed4ec8a5bed50f9da61ac78e6e
Nathan Grey <greynng@gmail.com>
2014-05-08 21:23:15 -0700

    add name to user model and small tweaks to navbar layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508173732) do
+ActiveRecord::Schema.define(version: 20140509041219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
