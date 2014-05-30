d4b2a8aa800c49a02eba1eeffc4b058b85d9ef1e
Nathan Grey <greynng@gmail.com>
2014-05-29 21:08:49 -0700

    small tweaks to formating

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,7 +53,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :date, :body, :image)
+    params.require(:post).permit(:title, :date, :body, :image, :date)
   end
 
 end
